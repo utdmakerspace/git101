8fb80a8339881c36b3c5a97f6642ba3934a72aa5
Shiva Sharma <sxs137030@utdallas.edu>
2017-01-30 20:38:47 -0600

    Shiva's commit - pull request

--- names/Shiva.js
+++ names/Shiva.js
@@ -1,10 +1,7 @@
 @@ -1,5 +1,5 @@
 function main() 
 {               
-    console.log("I'm about to commit a change"); 
-	console.log("Shiva is about to commit a change!!!"); 	
-	
-	
+    console.log("Shiva just sent a pull request!");            
 }   
 main()
 \ No newline at end of file
