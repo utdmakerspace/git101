d97b7b92f48eb49d381454fb4caf37282ae468d2
Ata Gowani <atagowani@gmail.com>
2017-01-30 19:47:44 -0600

    This is Ata's change.

--- names/Ata.js
+++ names/Ata.js
@@ -1,5 +1,5 @@
 function main() 
 {               
-    console.log("<yourName> just sent a pull request!");            
+    console.log("Ata Gowani just sent a pull request!");            
 }   
 main()
