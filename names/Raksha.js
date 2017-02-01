3ac6a18bf1ca376ec52d4f780cebeb7ff030b3cb
Raviteja Lingineni <rlingineni@live.com>
2017-01-31 20:14:39 -0600

    Update Raksha.js

--- names/Raksha.js
+++ names/Raksha.js
@@ -1,7 +1,7 @@
 function main() 
 {               
     console.log("I'm about to commit a change"); 
-    console.log("Raksha push request")  
+    console.log("Raksha pull request");  
 
     //this is Raksha's change         
 }   
